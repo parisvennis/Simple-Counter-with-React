f3c5b8981702d226f3bbd12922c9e181083e78e5
parisvennis <parisvennis@gmail.com>
2020-07-29 14:37:27 +0000

    updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,20 +5,18 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	var counter = 0;
+	counter += 1;
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div>0</div>
+			<div>0</div>
+			<div>0</div>
+			<div>0</div>
+			<div>0</div>
+			<div>0</div>
+			<div>0</div>
 		</div>
 	);
 }
--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,26 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+var counter = 0;
+var seconds;
+
+var myTimer = () => {
+	counter += 1;
+	seconds = "000000" + counter;
+	seconds = seconds.slice(-6);
+
+	ReactDOM.render(
+		<Home
+			char1={seconds.charAt(0)}
+			char2={seconds.charAt(1)}
+			char3={seconds.charAt(2)}
+			char4={seconds.charAt(3)}
+			char5={seconds.charAt(4)}
+			char6={seconds.charAt(5)}
+		/>,
+		document.querySelector("#app")
+	);
+};
+
+setInterval(myTimer, 1000);
